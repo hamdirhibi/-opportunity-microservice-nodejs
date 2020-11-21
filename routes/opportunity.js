67091f7be8b1f302ed8883d75d1eba20bf4a64e3
geekrhibi <geekrhibi@gmail.com>
2020-11-22 00:50:16 +0100

    opportunity $v1.1

---
+++ routes/opportunity.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const opportunityControlller = require("../controllers/opportunity");
+const checkAuth = require("../middleware/checkAuth");
+let upload = require('../config/multer.config.js');
+
+
+
+
+router.post("/:companyId", upload.any()  , checkAuth, opportunityControlller.newOpportunity);
+router.get("/company/:companyId", checkAuth ,  opportunityControlller.getOpportunitiesByCompany);
+router.delete("/:opportunityId", checkAuth ,  opportunityControlller.deleteOpportunity);
+router.put("/updateOne/:opportunityId", checkAuth ,  opportunityControlller.updateOpportunity);
+router.put("/updateOpportunityCover/:opportunityId", checkAuth ,opportunityControlller.updateOpportunityCover);
+router.put("/updateOpportunityDate/:opportunityId", checkAuth ,opportunityControlller.updateOpportunityDate);
+router.put("/updateOpportunityDuration/:opportunityId", checkAuth ,opportunityControlller.updateOpportunityDuration);
+
+
+
+module.exports = router ; 
+    
